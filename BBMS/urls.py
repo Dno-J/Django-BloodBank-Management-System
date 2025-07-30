321500c590030a3344ec460567d3d80462342c90
Dino Jackson <jacksondino00@gmail.com>
2025-07-30 23:51:43 +0530

    🛠️ Wire up homepage and healthz for Render

--- BBMS/urls.py
+++ BBMS/urls.py
@@ -1,9 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from django.http import HttpResponse
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name="homepage.html"), name='home'),  # 🚀 Homepage route
-    path('healthz/', lambda request: HttpResponse("OK"), name='healthz'),        # ❤️ Health check for Render
+    path('', TemplateView.as_view(template_name="homepage.html"), name='home'),
+    path('healthz/', lambda request: HttpResponse("OK"), name='healthz'),
 
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
@@ -11,7 +15,5 @@ urlpatterns = [
     path('captcha/', include('captcha.urls')),
 ]
 
-
-# Only serve media in development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
